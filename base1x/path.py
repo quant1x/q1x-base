666c45a76942c1119db75ae828501e7c3bccdda7
wangfeng <wangfengxy@sina.cn>
2023-11-20 08:05:37 +0800

    获取项目路径增加默认路径

--- base1x/path.py
+++ base1x/path.py
@@ -6,19 +6,21 @@ import sys
 namespace = "quant1x"
 
 
-def project_path() -> str:
-    pos = __file__.rfind(namespace)
-    project_path = __file__[:pos - 1]
+def project_path(filename: str = '') -> str:
+    if len(filename) == 0:
+        filename = __file__
+    pos = filename.rfind(namespace)
+    project_path = filename[:pos - 1]
     root_path = os.path.abspath(project_path)
     return root_path
 
 
-def redirect():
+def redirect(filename: str = ''):
     """
     重定向于当前工程目录
     :return:
     """
-    root_path = project_path()
+    root_path = project_path(filename)
     sys.path.insert(0, root_path)
 
 
