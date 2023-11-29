aa07726029ee23797ff73165f6ceed7ab3d4e402
wangfeng <wangfengxy@sina.cn>
2023-11-29 14:36:56 +0800

    简化path的函数引用路径

--- base1x/__init__.py
+++ base1x/__init__.py
@@ -7,4 +7,5 @@ from .base import TimeRange, TIME_FORMAT_TIMESTAMP
 from .base import TradingSession
 from .device import cpu_num, max_procs
 from .network import ip_is_private, ip_is_secure, lan_address
+from .path import project_path, redirect
 from .version import project_version
