54aed2a23ca644ec9b8258beac09cc5e8d60aaaf
wangfeng <wangfengxy@sina.cn>
2023-11-30 03:54:08 +0800

    调整简化引入的路径

--- base1x/__init__.py
+++ base1x/__init__.py
@@ -2,10 +2,10 @@
 
 __author__ = 'WangFeng'
 
-from .base import Singleton
-from .base import TimeRange, TIME_FORMAT_TIMESTAMP
-from .base import TradingSession
+from .datetime import TimeRange, FORMAT_DATETIME
+from .datetime import TradingSession
 from .device import cpu_num, max_procs
+from .devp import project_path, redirect
 from .network import ip_is_private, ip_is_secure, lan_address
-from .path import project_path, redirect
+from .pattern import Singleton
 from .version import project_version
