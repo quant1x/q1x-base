818df1d51b5339fd798db45e8350e0b736d4791a
wangfeng <wangfengxy@sina.cn>
2023-11-30 04:17:20 +0800

    修复路径可能存在非项目本地文件路径的问题

--- base1x/devp.py
+++ base1x/devp.py
@@ -2,25 +2,24 @@
 import os
 import sys
 
-# 项目package顶层命名空间
-namespace = "quant1x"
 
-
-def project_path(filename: str = '') -> str:
+def project_path(package: str = "quant1x", filename: str = '') -> str:
+    filename = filename.strip()
     if len(filename) == 0:
-        filename = __file__
-    pos = filename.rfind(namespace)
+        # filename = __file__
+        raise Exception('filename is empty')
+    pos = filename.rfind(package)
     project_path = filename[:pos - 1]
     root_path = os.path.abspath(project_path)
     return root_path
 
 
-def redirect(filename: str = ''):
+def redirect(package: str = "quant1x", filename: str = ''):
     """
     重定向于当前工程目录
     :return:
     """
-    root_path = project_path(filename)
+    root_path = project_path(package, filename)
     sys.path.insert(0, root_path)
 
 
