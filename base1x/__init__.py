26218af47a927bac74ff1f9876a24a36c6ab3bec
wangfeng <wangfengxy@sina.cn>
2023-11-30 08:07:18 +0800

    修改浮点四舍五入的函数名

--- base1x/__init__.py
+++ base1x/__init__.py
@@ -6,7 +6,7 @@ from .device import cpu_num, max_procs
 from .devp import project_path, redirect
 from .file import mkdirs, touch
 from .network import ip_is_private, ip_is_secure, lan_address
-from .num import is_nan, price_round, fix_float
+from .num import is_nan, float_round, fix_float
 from .pattern import Singleton
 from .timestamp import TimeRange, FORMAT_DATETIME
 from .timestamp import TradingSession
