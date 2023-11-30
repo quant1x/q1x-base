4a05292219e66b850a5f60adb48a1ceed8f9b509
wangfeng <wangfengxy@sina.cn>
2023-11-30 08:11:02 +0800

    增加日志工具

--- base1x/__init__.py
+++ base1x/__init__.py
@@ -5,6 +5,7 @@ __author__ = 'WangFeng'
 from .device import cpu_num, max_procs
 from .devp import project_path, redirect
 from .file import mkdirs, touch
+from .logger import logger
 from .network import ip_is_private, ip_is_secure, lan_address
 from .num import is_nan, float_round, fix_float
 from .pattern import Singleton
---
+++ base1x/logger.py
@@ -0,0 +1,4 @@
+# -*- coding: UTF-8 -*-
+from loguru import logger as __logger
+
+logger = __logger
