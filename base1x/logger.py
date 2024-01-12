ee4c57f5c9783d437f50e98c2adb5da43584f2c6
mephisto <mephistophilis@gmail.com>
2024-01-12 21:04:11 +0800

    根据app名字初始化logger

--- base1x/logger.py
+++ base1x/logger.py
@@ -1,4 +1,9 @@
 # -*- coding: UTF-8 -*-
 from loguru import logger as __logger
 
+from .application import application
+splitted_app_path = application()
+log_file = f"{splitted_app_path[0]}/{splitted_app_path[1]}.log"
+__logger.add(log_file, rotation="00:00", retention="10 days")
+
 logger = __logger
