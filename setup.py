92bd97856b8db05aaa80f9182267b21fb10e7dbc
wangfeng <wangfengxy@sina.cn>
2025-06-20 08:26:35 +0800

    支持python3.12

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     data_files=[
         # ('xtquant', ['xtquant/xtdata.ini', 'xtquant/xtdata.log4cxx']),
