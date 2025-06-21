9bc5348b31d18061281154381b32c30d2a9d33ff
wangfeng <wangfengxy@sina.cn>
2025-06-21 09:32:59 +0800

    num模块改名为numerics

--- q1x/base/__init__.py
+++ q1x/base/__init__.py
@@ -8,7 +8,7 @@ from .devp import project_path, redirect
 from .file import mkdirs, touch, homedir
 from .logger import logger
 from .network import ip_is_private, ip_is_secure, lan_address
-from .num import is_nan, float_round, fix_float
+from .numerics import is_nan, float_round, fix_float
 from .pattern import Singleton
 from .timestamp import TimeRange, FORMAT_DATETIME
 from .timestamp import TradingSession, seconds_to_timestamp
