18d5131e5c8463a91c9fe4a02e6905d051434841
wangfeng <wangfengxy@sina.cn>
2025-08-04 05:50:47 +0800

    增加盘前集合竞价测试代码

---
+++ q1x/core/premarket.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Project : q1x-base
+@Package : core
+@File    : premark.py
+@Author  : wangfeng
+@Date    : 2025/8/2 7:54
+@Desc    : 盘前集合竞价
+"""
+
+import akshare as ak
+stock_zh_a_hist_pre_min_em_df = ak.stock_zh_a_hist_pre_min_em(symbol="000158")
+print(stock_zh_a_hist_pre_min_em_df)
