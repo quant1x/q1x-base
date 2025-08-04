2647069dc72cc04d3b6bd2c5be9e4c00853e666b
wangfeng <wangfengxy@sina.cn>
2025-08-04 14:12:57 +0800

    调整测试代码

--- q1x/core/option_test.py
+++ q1x/core/option_test.py
@@ -8,16 +8,18 @@
 @Date    : 2025/7/31 8:41
 @Desc    : 期权数据 - 测试
 """
+from q1x.core import option
 import akshare as ak
 
-option_finance_board_df = ak.option_finance_board(symbol="华泰柏瑞沪深300ETF期权", end_month="2512")
+option_finance_board_df = option.option_finance_board(symbol="华泰柏瑞沪深300ETF期权", end_month="2306")
 print(option_finance_board_df.columns)
 print('------')
 print(option_finance_board_df)
 
-import akshare as ak
-
-option_risk_indicator_sse_df = ak.option_risk_indicator_sse(date="20250731")
+option_risk_indicator_sse_df = option.option_risk_indicator_sse(date="20250731")
 option_risk_indicator_sse_df.to_csv("option_risk_indicator_sse.csv")
 print(option_risk_indicator_sse_df.columns)
 print(option_risk_indicator_sse_df)
+
+# option_daily_stats_sse_df = ak.option_daily_stats_sse(date="20240626")
+# print(option_daily_stats_sse_df)
